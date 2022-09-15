5c4a45e6cee2e794afbd3131bab20fa6c695a5fb
Illya Gorbenko <ilyagvv@gmail.com>
2022-09-15 12:24:00 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -142,6 +142,43 @@ class Dev(Configuration):
   CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
   CRISPY_TEMPLATE_PACK = "bootstrap5"
 
+  LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+    },
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler",
+         "stream": "ext://sys.stdout",
+         "formatter": "verbose",},
+        "mail_admins": {
+         "level": "ERROR",
+         "class": "django.utils.log.AdminEmailHandler",
+         "filters": ["require_debug_false"],
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    }
+}
+
 
 class Prod(Dev):
   DEBUG = values.BooleanValue(True)
