7a50270ba65d094abce471f25a93c83055669fbf
Illya Gorbenko <ilyagvv@gmail.com>
2022-09-18 06:21:50 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -49,6 +49,7 @@ class Dev(Configuration):
       'django.contrib.sessions',
       'django.contrib.messages',
       'django.contrib.staticfiles',
+      'blango_auth',
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
@@ -67,6 +68,8 @@ class Dev(Configuration):
   ]
 
   INTERNAL_IPS = ["192.168.11.179"]
+
+  AUTH_USER_MODEL = "blango_auth.User"
 
   ROOT_URLCONF = 'blango.urls'
 
